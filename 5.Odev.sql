9f30da2db5c1774c42e9ebe196a2db24063c81e0
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-23 20:35:24 +0300

    Add files via upload

---
+++ 5.Odev.sql
@@ -0,0 +1,21 @@
+--1.Uygulama:
+
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+--2.Uygulama:
+
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length 
+OFFSET 5
+LIMIT 5;
+
+--3. Uygulama:
+
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
